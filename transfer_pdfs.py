a713508236238f94cc058b59764199547027eb04
Aleksandar Josifoski <josifoski@gmail.com>
2017-02-19 20:29:59 +0100

    update

--- transfer_pdfs.py
+++ transfer_pdfs.py
@@ -1,23 +1,10 @@
-#! /usr/bin/env python3.5
+#! /usr/bin/env python3.6
 # Script for downloading zip files from specific url, extracting pdfs from, then renaming files and uploading via FTP
-# Marc Joffe https://github.com/joffemd
-# Aleksandar Josifoski https://github.com/josifoski
+# Aleksandar Josifoski https://about.me/josifsk
 # Script is dependend on selenium, pyvirtualdisplay, BeautifulSoup4, openpyxl
 # pip install -U selenium pyvirtualdisplay BeautifulSoup4 openpyxl
 # Also is dependend on geckodriver. Explanations for geckodriver few lines bellow
 # 2017 February 19
-
-url = 'https://harvester.census.gov/facdissem/SearchA133.aspx'
-rangefrom = '02/07/2017'
-rangeto = '02/09/2017'
-dir_in = '/data/upwork/Marc_Joffe/'  # directory where script will be
-dir_downloads = '/data/upwork/Marc_Joffe/downloads/'
-dir_pdfs = '/data/upwork/Marc_Joffe/pdfs/' # make sure that all directories are previosly created
-
-fileshortnames = 'ShortOutputNames_20170217.xlsx' # place this file in dir_in directory
-sheetShortName = 'Sheet1'
-
-headlessMode = False # True is for invisible firefox, while False here will show firefox visible
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
@@ -41,6 +28,22 @@ import openpyxl
 import json
 from ftplib import FTP
 from ftplib import FTP_TLS
+import ntpath
+
+with open('parameters.txt', 'r') as fp:
+    dparameters = json.load(fp)
+    
+print(dparameters)
+sys.exit()
+url = dparameters["url"]
+rangefrom = dparameters["rangefrom"]
+rangeto = dparameters["rangeto"]
+dir_in = dparameters["dir_in"]
+dir_downloads = dparameters["dir_downloads"]
+dir_pdfs = dparameters["dir_pdfs"]
+fileshortnames = dparameters["fileshortnames"]
+sheetShortName = dparameters["sheetShortName"]
+headlessMode = dparameters["headlessMode"]
 
 # for selenium to work properly, geckodriver is needed to be downloaded,
 # placed in some directory and in next line starting with 
@@ -138,15 +141,14 @@ def download():
         bnum = False
 
     def is_download_completed():
-        time.sleep(30)
+        time.sleep(dparameters["sleeptime"])
         while True:
             l = glob.glob(dir_downloads + '*.part')
             if len(l) == 0:
                 # print'Downloading ' + audit + ' completed')
                 break
             else:
-                time.sleep(30)
-                # print('sleeping 30 seconds')
+                time.sleep(dparameters["sleeptime"])
 
     if bnum:
         # in this for loop we are selecting by groups of 100
@@ -161,23 +163,7 @@ def download():
         display.stop()
 
 def ftp_upload_pdfs():
-    ''' function for uploading pdf files to FTP server
-    connect to server via parameters file
-    FTP_parameters.txt file should be json like
-    don't forget to put ftp. as prefix for server
-    for port put integer value, recommended 21
-    {
-        "server"  : "",
-        "port"    : ,
-        "username": "",
-        "password": ""
-    }
-    '''
-    
-    with open(dir_in + 'FTP_parameters.txt', 'r') as fp:
-        dparameters = json.load(fp)
-    # print(dparameters)
-    
+    ''' function for uploading pdf files to FTP server '''
     # get a list of pdf files in dir_pdfs
     lpdfs = glob.glob(dir_pdfs + "*.pdf")
     lpdfs.sort()
@@ -193,7 +179,7 @@ def ftp_upload_pdfs():
         print("Connection to ftp successfully established...")
         #ftp.cwd('path_to_destination_directory_if_needed_on_server')
         for pdffile in lpdfs:
-            rpdffile = pdffile.split('/')[-1]
+            rpdffile = ntpath.basename(pdffile)
             print('uploading ' + rpdffile)
             logging.info('uploading ' + rpdffile)
             ffile = open(pdffile, 'rb')
@@ -239,7 +225,6 @@ def extract_and_rename():
             row = zrow + 2
             if sheetCross['A' + str(row)].value == None: 
                 break
-            # sheet['A1'].value
             lfilename = sheetCross['B' + str(row)].value.strip()
             lauditeename = sheetCross['C' + str(row)].value.strip()
             lstate = sheetCross['E' + str(row)].value.strip()
@@ -250,16 +235,29 @@ def extract_and_rename():
             lname = dshort.get(sheetCross['F' + str(row)].value.strip(), sheetCross['C' + str(row)].value.strip())
             
             # filterling lname from special characters
-            lname = lname.replace('/', '_slash_') # this line is must. You can change _slash_ to some other string
-            
-            # You can eventually polish filenames if you preffer commenting out some of next replace commands
-            #lname = lname.replace("'", "")
-            #lname = lname.replace('"', '')
-            #lname = lname.replace(',', '')
-            #lname = lname.replace('&', '')
-            #lname = lname.replace('.', '')
-            #lname = lname.replace('(', '')
-            #lname = lname.replace(')', '')
+            lname = lname.replace('/', '_') # this line is must. You can change _slash_ to some other string
+            lname = lname.replace(':', '_')
+            lname = lname.replace('\\', '')
+            lname = lname.replace("'", "_")
+            lname = lname.replace('"', '_')
+            lname = lname.replace(',', '')
+            lname = lname.replace('&', '')
+            lname = lname.replace('.', '')
+            lname = lname.replace('#', '')
+            lname = lname.replace('%', '')
+            lname = lname.replace('{', '')
+            lname = lname.replace('}', '')
+            lname = lname.replace('<', '')
+            lname = lname.replace('>', '')
+            lname = lname.replace('*', '')
+            lname = lname.replace('?', '')
+            lname = lname.replace('$', '')
+            lname = lname.replace('!', '')
+            lname = lname.replace('@', '')
+            lname = lname.replace('+', '')
+            lname = lname.replace('`', '')
+            lname = lname.replace('|', '')
+            lname = lname.replace('=', '')
             
             try:
                 os.rename(dir_pdfs + lfilename + '.pdf', dir_pdfs + lstate + ' ' + lname + ' ' + lyearending + '.pdf')
@@ -267,25 +265,14 @@ def extract_and_rename():
                 print(str(e))
                 logging.debug(str(e))
             
-            # print(lfilename)
-            # print(lauditeename)
-            # print(lstate)
-            # print(lein)
-            # print(lyearending)
             print((lfilename + '.pdf').ljust(20) + lname + '.pdf')
             logging.info((lfilename + '.pdf').ljust(20) + lname + '.pdf')
-            #littlestop = input('press enter')
-        
-        # small sleep for system to affect rename command for all files
-        time.sleep(10)
-        ftp_upload_pdfs()
-        # current zip file completed
-        # you can delete it with uncommenting next line
-        # os.remove(myzipfile)
-        # Please note, script is processing ALL zip files, and all pdf files in appropriate directories
-        # if you leave zip files, they will be again processed.
-        # But knowing that donwload speed from server is slowish, temporary I am leaving above command with comment.
 
+        # sleep for system to affect rename command for all files
+        time.sleep(dparameters["sleeptime"])
+        ftp_upload_pdfs()
+        os.remove(myzipfile)
+ 
 def calculate_time():
     time2 = time.time()
     hours = int((time2-time1)/3600)
