9e9e748adb09d452358d753511c9aab4f894f1d3
Aleksandar Josifoski <josifoski@gmail.com>
2017-02-21 06:33:55 +0100

    added download.py

--- download.py
+++ download.py
@@ -1,11 +1,3 @@
-#! /usr/bin/env python3.6
-# Script for downloading zip files from specific url, extracting pdfs from, then renaming files and uploading via FTP
-# Aleksandar Josifoski https://about.me/josifsk
-# Script is dependend on selenium, pyvirtualdisplay, BeautifulSoup4, openpyxl
-# pip install -U selenium pyvirtualdisplay BeautifulSoup4 openpyxl
-# Also is dependend on geckodriver. Explanations for geckodriver few lines bellow
-# 2017 February 19
-
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -159,120 +151,7 @@ def download():
     driver.close()
     if headlessMode:
         display.stop()
-
-def ftp_upload_pdfs():
-    ''' function for uploading pdf files to FTP server '''
-    # get a list of pdf files in dir_pdfs
-    lpdfs = glob.glob(dir_pdfs + "*.pdf")
-    lpdfs.sort()
-    os.chdir(dir_pdfs) # needed for ftp.storbinary('STOR command work not with paths but with filenames
-    
-    # connect to FTP server and upload files
-    try:
-        ftp = FTP()
-        # ftp = FTP_TLS()
-        ftp.connect(dparameters["server"].strip(), dparameters["port"])
-        ftp.login(user = dparameters["username"].strip(), passwd = dparameters["password"].strip())
-        # ftp.prot_p() if using FTP_TLS uncomment this line
-        print("Connection to ftp successfully established...")
-        #ftp.cwd('path_to_destination_directory_if_needed_on_server')
-        for pdffile in lpdfs:
-            rpdffile = ntpath.basename(pdffile)
-            print('uploading ' + rpdffile)
-            logging.info('uploading ' + rpdffile)
-            ffile = open(pdffile, 'rb')
-            ftp.storbinary('STOR ' + rpdffile, ffile)
-            ffile.close()
-            # file uploaded delete it now
-            os.remove(pdffile)
-        ftp.quit()
-    except Exception as e:
-        print(str(e))
-        logging.critical(str(e))
-
-def extract_and_rename():
-    ''' function for extracting zip files and renaming pdf files'''
-    print('Extracting files..')
-    lloc = glob.glob(dir_downloads + '*.zip')
-    lloc.sort()
-    
-    if len(lloc) == 0:
-        print('no zip file(s). quiting')
-        logging.info('no zip file(s). quiting')
-    
-    # placing shortnames in dictionary
-    wbShort = openpyxl.load_workbook(dir_in + fileshortnames.strip())
-    sheetShort = wbShort.get_sheet_by_name(sheetShortName.strip())
-    dshort = {}
-    row = 2
-    scrolldown = True
-    while scrolldown:
-        dshort[sheetShort['A' + str(row)].value.strip()] = sheetShort['D' + str(row)].value.strip()
-        row += 1
-        if sheetShort['A' + str(row)].value == None:
-            scrolldown = False # when finding empty row parsing of Shortnames xlsx will stop
-    
-    for myzipfile in lloc:
-        with zipfile.ZipFile(myzipfile, "r") as z:
-            z.extractall(dir_pdfs)
-        # here comes part for renaming
-        print('Renaming files..')
-        wbCross = openpyxl.load_workbook(dir_pdfs + 'FileNameCrossReferenceList.xlsx')
         
-        sheetCross = wbCross.get_sheet_by_name('Table1')
-        for zrow in range(99):
-            row = zrow + 2
-            if sheetCross['A' + str(row)].value == None: 
-                break
-            lfilename = sheetCross['B' + str(row)].value.strip()
-            lauditeename = sheetCross['C' + str(row)].value.strip()
-            lstate = sheetCross['E' + str(row)].value.strip()
-            lein = sheetCross['F' + str(row)].value.strip()
-            lyearending = sheetCross['G' + str(row)].value.split('/')[-1].strip()
-            # try to find short output name
-            # in case there is in lshortname will be appended shortened name else original auditee name
-            lname = dshort.get(sheetCross['F' + str(row)].value.strip(), sheetCross['C' + str(row)].value.strip())
-            
-            # filterling lname from special characters
-            lname = lname.replace('/', '_')
-            lname = lname.replace(':', '_')
-            lname = lname.replace('\\', '')
-            lname = lname.replace("'", "_")
-            lname = lname.replace('"', '_')
-            lname = lname.replace(',', '')
-            lname = lname.replace('&', '')
-            lname = lname.replace('.', '')
-            lname = lname.replace('#', '')
-            lname = lname.replace('%', '')
-            lname = lname.replace('{', '')
-            lname = lname.replace('}', '')
-            lname = lname.replace('<', '')
-            lname = lname.replace('>', '')
-            lname = lname.replace('*', '')
-            lname = lname.replace('?', '')
-            lname = lname.replace('$', '')
-            lname = lname.replace('!', '')
-            lname = lname.replace('@', '')
-            lname = lname.replace('+', '')
-            lname = lname.replace('`', '')
-            lname = lname.replace('|', '')
-            lname = lname.replace('=', '')
-            
-            try:
-                os.rename(dir_pdfs + lfilename + '.pdf', dir_pdfs + lstate + ' ' + lname + ' ' + lyearending + '.pdf')
-                time.sleep(0.3)
-            except Exception as e:
-                print(str(e))
-                logging.debug(str(e))
-            
-            print((lfilename + '.pdf').ljust(20) + lname + '.pdf')
-            logging.info((lfilename + '.pdf').ljust(20) + lname + '.pdf')
-
-        # sleep for system to affect rename command for all files
-        time.sleep(dparameters["sleeptime"])
-        ftp_upload_pdfs()
-        os.remove(myzipfile)
- 
 def calculate_time():
     time2 = time.time()
     hours = int((time2-time1)/3600)
@@ -282,7 +161,5 @@ def calculate_time():
 
 if __name__ == '__main__':
     download()
-    extract_and_rename() # since FileNameCrossReferenceList.xlsx is same for all groups, 
-                         # script have to use this grouped approaching, processing them by 100 or less for final group
     calculate_time()
     print('Done.')
