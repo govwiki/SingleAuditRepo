3b8e76854516ccd391ed6f21bf4b85c647a313da
artyom-smushkov <artyom.smushkov@gmail.com>
2017-12-21 21:17:32 +0200

    Georgia scraper

---
+++ get_GA.py
@@ -0,0 +1,51 @@
+import argparse
+import configparser
+import os
+import sys
+import urllib.parse
+import urllib.request
+from utils import Crawler
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("start_year")
+    argparser.add_argument("end_year")
+    args = argparser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+
+    downloads_path = config.get('general', 'downloads_path', fallback='/tmp/downloads/')
+    if not os.path.exists(downloads_path):
+        os.makedirs(downloads_path)
+    elif not os.path.isdir(downloads_path):
+        print('ERROR: downloads_path parameter points to file!')
+        sys.exit(1)
+
+    crawler = Crawler(config)
+    crawler.get(config.get('georgia', 'url'))
+    crawler.select_option('#edit-field-fiscal-year-value-min-year', args.start_year)
+    crawler.select_option('#edit-field-fiscal-year-value-max-year', args.end_year)
+    last_option = 0
+    option_selected = True
+    while option_selected:
+        crawler.deselect_all('select[multiple="multiple"]')
+        for i in range(last_option, last_option + 10):
+            option_selected = crawler.select_option_by_index('select[multiple="multiple"]', i)
+        last_option += 10
+        crawler.click('#edit-submit-financial-documents-advanced')
+        all_pages_crawled = False
+        while not all_pages_crawled:
+            for url in crawler.get_attr('.file a', 'href', single=False):
+                print('Downloading', url)
+                try:
+                    r = urllib.request.urlopen(url)
+                    with open(os.path.join(config.get('general', 'downloads_path', fallback='/tmp/downloads/'), urllib.parse.unquote(url).split('/')[-1]), 'wb') as f:
+                        f.write(r.read())
+                except:
+                    print('ERROR: Downloading failed!')
+            try:
+                crawler.click('.pager-next a')
+            except Exception:
+                all_pages_crawled = True
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+import glob
+import os
 import time
+from ftplib import FTP
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 class Crawler:
@@ -51,5 +55,67 @@ class Crawler:
             return self.browser.find_element_by_css_selector(selector).get_attribute(attr)
         return [el.get_attribute(attr) for el in self.browser.find_elements_by_css_selector(selector)]
 
+    def execute(self, script):
+        self.browser.execute_script(script, [])
+        time.sleep(3)
+
+    def deselect_all(self, selector):
+        select = Select(self.browser.find_element_by_css_selector(selector))
+        select.deselect_all()
+        time.sleep(3)
+
+    def select_option(self, selector, option):
+        select = Select(self.browser.find_element_by_css_selector(selector))
+        select.select_by_visible_text(option)
+        time.sleep(3)
+
+    def select_option_by_index(self, selector, index):
+        select = Select(self.browser.find_element_by_css_selector(selector))
+        if index < len(select.options):
+            select.select_by_index(index)
+            time.sleep(3)
+            return True
+        return False
+
     def close(self):
         self.browser.quit()
+
+    def _get_remote_filename(local_filename):
+        raise NotImplemented
+
+    def upload_to_ftp(self):
+        pdf_paths = glob.glob(self.downloads_path + "*.pdf")
+        pdf_paths.sort()
+
+        try:
+            ftp = FTP()
+            ftp.connect(
+                self.config.get('general', 'ftp_server').strip(),
+                int(self.config.get('general', 'ftp_port')),
+            )
+            ftp.login(
+                user=self.config.get('general', 'ftp_username').strip(),
+                passwd=self.config.get('general', 'ftp_password').strip(),
+            )
+            # ftp.prot_p() if using FTP_TLS uncomment this line
+            print("Connection to ftp successfully established...")
+
+            for path in pdf_paths:
+                pdf_filename = os.path.basename(path)
+                pdf_file = open(path, 'rb')
+                remote_filename = self._get_remote_filename(pdf_filename)
+                if not remote_filename:
+                    continue
+                directory, filename = remote_filename
+                if not self.config.getboolean('virginia', 'overwrite_remote_files', fallback=False):
+                    if filename in [f[0] for f in ftp.mlsd(directory)]:
+                        continue
+                import ipdb
+                ipdb.set_trace()
+                ftp.cwd('/{}'.format(directory))
+                ftp.storbinary('STOR {}'.format(filename), pdf_file)
+                pdf_file.close()
+                break
+            ftp.quit()
+        except Exception as e:
+            print(str(e))
