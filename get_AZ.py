1a3747690c6e27ae97087f98a94c745aa30d2358
artyom-smushkov <artyom.smushkov@gmail.com>
2018-01-06 22:36:19 +0200

    Upload Arizona files

--- get_AZ.py
+++ get_AZ.py
@@ -1,6 +1,18 @@
 import configparser
-import urllib.parse
-from utils import Crawler
+from utils import Crawler as CoreCrawler
+
+
+class Crawler(CoreCrawler):
+    abbr = 'AZ'
+
+    def _get_remote_filename(self, local_filename):
+        entity_name, entity_type, year = local_filename[:-4].split('|')
+        if entity_type == 'County':
+            directory = 'General Purpose'
+        else:
+            directory = 'Community College Districts'
+        filename = '{} {} {}.pdf'.format(self.abbr, entity_name, year)
+        return directory, filename
 
 
 if __name__ == '__main__':
@@ -8,6 +20,7 @@ if __name__ == '__main__':
     config.read('conf.ini')
 
     crawler = Crawler(config, 'arizona')
+    entity_type = 'County'
     for state_url in config.get('arizona', 'urls').split('\n'):
         crawler.get(state_url.strip())
         while True:
@@ -15,8 +28,16 @@ if __name__ == '__main__':
                 row_type = crawler.get_text('.views-field-field-audit-type', root=row)
                 if 'financial audit' in row_type.lower() or 'single audit' in row_type.lower():
                     url = crawler.get_attr('strong a', 'href', root=row)
-                    crawler.download(url, urllib.parse.unquote(url).split('/')[-1])
+                    if entity_type == 'County':
+                        text = crawler.get_text('.views-field.views-field-field-counties', root=row)
+                    else:
+                        text = crawler.get_text('.views-field.views-field-field-community-college', root=row)
+                    year = crawler.get_text('span[datatype="xsd:dateTime"]', root=row)[-4:]
+                    crawler.download(url, '{}|{}|{}.pdf'.format(text, entity_type, year))
             try:
-                crawler.click('.next a')
+                crawler.get(crawler.get_attr('.next a', 'href'))
             except Exception:
                 break
+        entity_type = 'Community College'
+    crawler.upload_to_ftp()
+    crawler.close()
