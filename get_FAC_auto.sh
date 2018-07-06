eb776af96ed4acb1729e41fdda7690c0d7e68924
Michael Rusaleev <michael.rusaleev@sibers.com>
2018-07-06 22:14:24 +0700

    different scripts for manual and automatic runs

--- get_FAC_auto.sh
+++ get_FAC_auto.sh
@@ -1,3 +1,9 @@
+set -x
+set -v
+#get current date
+TODAY=`date +%m/%d/%Y`
+YESTERDAY=`date +%m/%d/%Y -d "3 day ago"`
+
 #change working folder
 cd /home/sibers/python_scripts/SingleAuditRepo
 echo changed workdir
@@ -7,9 +13,11 @@ source ./demoenv/bin/activate
 echo virtualenv activated
 
 # run the script
-echo running get_FAC_SA.py
-python get_FAC_SA.py
+echo running get_FAC_SA.py $YESTERDAY $TODAY
+python get_FAC_SA.py $YESTERDAY $TODAY
 
 #deactivate virtualenv
 deactivate
 echo virtualenv deactivated
+set +x
+set +v
