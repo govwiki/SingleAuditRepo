2f9b7d2e0b04c792e6310ce9121e3e95cd279803
Michael Rusaleev <michael.rusaleev@sibers.com>
2019-08-22 15:10:59 +0700

    added get_CT.sh script

---
+++ get_CT.sh
@@ -0,0 +1,22 @@
+set -x
+set -v
+#get current date
+YESTERDAY=`date +%m/%d/%Y -d "3 day ago"`
+
+#change working folder
+cd /home/sibers/python_scripts/SingleAuditRepo
+echo changed workdir
+
+# activate virtualenv
+source ./demoenv/bin/activate
+echo virtualenv activated
+
+# run the script
+echo running get_CT.py $YESTERDAY
+python get_CT.py $YESTERDAY
+
+#deactivate virtualenv
+deactivate
+echo virtualenv deactivated
+set +x
+set +v
