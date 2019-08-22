0b41b8dac6b523bf83eae3d92fe5ce5cad748385
Michael Rusaleev <michael.rusaleev@sibers.com>
2019-08-22 15:31:44 +0700

    updated SQL scripts to match the actual production DB structure

--- SQL/add_CT_params.sql
+++ SQL/add_CT_params.sql
@@ -1,5 +1,13 @@
 INSERT INTO script_mapping (name,executable,param_categories) VALUES ('get_CT.py', 'TZ="America/Los_Angeles" /home/sibers/python_scripts/SingleAuditRepo/get_CT.sh', 'connecticut,general');
 
+-- general
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'headless_mode', 'True', 'for invisible (True) or visible (False) firefox');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'fs_server', 'https://cafr.file.core.windows.net', 'File Storage URL');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'fs_username', 'cafr', 'File Storage username');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'fs_password', 'OsA9Q0AHx1dNG2CZEyRxRyUL3XL7DMpChsNBYW8yzmSJOXIZNL2gDtELb/q72PZ4wODl5WITaCxqL6iI+tv0pw==', 'File Storage password');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'fs_share', 'cafr', 'File Storage share name');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'fs_directory_prefix', 'Unclassified/test_ct', 'File Storage base folder for uploads (leave blank to upload to upload to general storage)');
+
 -- connecticut
 INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'url', 'https://www.appsvcs.opm.ct.gov/Auditing/Public/Report.aspx', 'target url');
 INSERT INTO script_parameters (category, `key`, value, description) VALUES ('connecticut', 'downloads_path', '/tmp/downloads/CT/', 'temp (local) folder for file downloads');
--- SQL/add_FL_params.sql
+++ SQL/add_FL_params.sql
@@ -1,12 +1,12 @@
 INSERT INTO script_mapping (name,executable,param_categories) VALUES ('get_FL.py', 'TZ="America/Los_Angeles" /home/sibers/python_scripts/SingleAuditRepo/get_FL.sh', 'florida,general');
 
 -- general
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'headless_mode', 'True', 'for invisible (True) or visible (False) firefox');
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'fs_server', 'https://cafr.file.core.windows.net', 'File Storage URL');
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'fs_username', 'cafr', 'File Storage username');
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'fs_password', 'OsA9Q0AHx1dNG2CZEyRxRyUL3XL7DMpChsNBYW8yzmSJOXIZNL2gDtELb/q72PZ4wODl5WITaCxqL6iI+tv0pw==', 'File Storage password');
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'fs_share', 'cafr', 'File Storage share name');
-INSERT INTO script_parameters (category, `key`, value, description) VALUES ('general', 'fs_directory_prefix', 'test', 'File Storage base folder for uploads (leave blank to upload to upload to general storage)');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'headless_mode', 'True', 'for invisible (True) or visible (False) firefox');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'fs_server', 'https://cafr.file.core.windows.net', 'File Storage URL');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'fs_username', 'cafr', 'File Storage username');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'fs_password', 'OsA9Q0AHx1dNG2CZEyRxRyUL3XL7DMpChsNBYW8yzmSJOXIZNL2gDtELb/q72PZ4wODl5WITaCxqL6iI+tv0pw==', 'File Storage password');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'fs_share', 'cafr', 'File Storage share name');
+INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'fs_directory_prefix', '', 'File Storage base folder for uploads (leave blank to upload to upload to general storage)');
 
 -- florida
 INSERT INTO script_parameters (category, `key`, value, description) VALUES ('florida', 'urls', 'https://flauditor.gov/pages/municipalities_efiles.html
