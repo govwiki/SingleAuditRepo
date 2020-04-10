e89135314e42c6ea9dafa53181ba78867a934354
kharchenko <andrey.kharchenko@sibers.com>
2020-04-10 19:45:12 +0700

    code sync with demo

---
+++ file_checker_sd.sh
@@ -0,0 +1,18 @@
+set -x
+set -v
+cd /home/sibers/python_scripts/SingleAuditRepo
+echo changed workdir
+
+# activate virtualenv
+source ./demoenv/bin/activate
+echo virtualenv activated
+
+# run the script
+echo running file_checker_gp.py
+python file_checker_sd.py
+
+#deactivate virtualenv
+deactivate
+echo virtualenv deactivated
+set +x
+set +v
